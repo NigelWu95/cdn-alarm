8b6d6b90a1270c4a8df5caba397a4cd772d6e025
wubingheng <wubinghengajw@outlook.com>
2019-01-23 11:42:48 +0800

    add Refresh.

--- src/main/java/com/qiniu/nigel/cdn/Refresh.java
+++ src/main/java/com/qiniu/nigel/cdn/Refresh.java
@@ -1,4 +1,4 @@
-package com.qiniu.nigel;
+package com.qiniu.nigel.cdn;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -8,15 +8,45 @@ import com.qiniu.http.Response;
 import com.qiniu.util.Auth;
 import com.qiniu.util.StringMap;
 
-public class QiniuCdn {
+public class Refresh {
 
     private Client client;
+    private String urlQuotaDay;
+    private int urlSurplusDay;
+    private int dirQuotaDay;
+    private int dirSurplusDay;
+    private int quotaDay;
+    private int surplusDay;
 
-    public QiniuCdn() {
+    public Refresh() {
         client = new Client();
     }
 
-    public String queryCdnAmounts(Auth auth) throws QiniuException {
+    public String getUrlQuotaDay() {
+        return urlQuotaDay;
+    }
+
+    public int getUrlSurplusDay() {
+        return urlSurplusDay;
+    }
+
+    public int getDirQuotaDay() {
+        return dirQuotaDay;
+    }
+
+    public int getDirSurplusDay() {
+        return dirSurplusDay;
+    }
+
+    public int getQuotaDay() {
+        return quotaDay;
+    }
+
+    public int getSurplusDay() {
+        return surplusDay;
+    }
+
+    public int queryUrlSurplus(Auth auth) throws QiniuException {
         String url = "http://fusion.qiniuapi.com/refresh/user/surplus";
         String authorization = "QBox " + auth.signRequest(url, null, Client.FormMime);
         StringMap headers = new StringMap().put("Authorization", authorization);
@@ -26,7 +56,7 @@ public class QiniuCdn {
         int code = jsonObject.get("code").getAsInt();
         String error = jsonObject.get("error").getAsString();
         if (code != 200 || !"success".equals(error)) throw new QiniuException(response);
-        else
-        return "";
+        else urlSurplusDay = jsonObject.get("").getAsInt();
+        return urlSurplusDay;
     }
 }
